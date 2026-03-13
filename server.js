4f447e07b3d9c5637ac7f97f499e8106e350e9ac
VLJNH-VN <vljnh5326@gmail.com>
2026-03-14 03:08:38 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const { downloadVideo } = require("./downloader")
 const app = express()
 
 app.get("/downall", async (req, res) => {
+
   try {
+
     const url = req.query.url
 
     if (!url) {
-      return res.status(400).json({
+      return res.json({
         error: "url required"
       })
     }
@@ -16,62 +18,21 @@ app.get("/downall", async (req, res) => {
     await downloadVideo(url)
 
     res.json({
-      success: true
+      status: "download complete"
     })
 
   } catch (err) {
-    res.status(500).json({
-      error: err.message
-    })
-  }
-})
-
-app.listen(3000, () => {
-  console.log("API running on port 3000")
-})
-/*const express = require("express");
-const youtubedl = require("youtube-dl-exec");
-const path = require("path");
-const fs = require("fs");
-
-const app = express();
-app.use(express.json());
-
-const DOWNLOAD_DIR = path.join(__dirname, "downloads");
-
-if (!fs.existsSync(DOWNLOAD_DIR)) {
-  fs.mkdirSync(DOWNLOAD_DIR);
-}
-
-// API download
-app.post("/downall", async (req, res) => {
-  try {
-    const { url } = req.body;
-
-    if (!url) {
-      return res.status(400).json({ error: "URL required" });
-    }
-
-    const filePath = path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s");
-
-    await youtubedl(url, {
-      output: filePath,
-      format: "best"
-    });
 
     res.json({
-      status: "success",
-      message: "Download completed"
-    });
-
-  } catch (err) {
-    res.status(500).json({
       error: err.message
-    });
-  }
-});
+    })
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
-*/
+  }
+
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log("Server running")
+})
