f8604041a796b1f2e8eb767188951b6dc8933584
VLJNH-VN <vljnh5326@gmail.com>
2026-03-14 03:02:14 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,35 @@
-const express = require("express");
+const express = require("express")
+const { downloadVideo } = require("./downloader")
+
+const app = express()
+
+app.get("/downall", async (req, res) => {
+  try {
+    const url = req.query.url
+
+    if (!url) {
+      return res.status(400).json({
+        error: "url required"
+      })
+    }
+
+    await downloadVideo(url)
+
+    res.json({
+      success: true
+    })
+
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    })
+  }
+})
+
+app.listen(3000, () => {
+  console.log("API running on port 3000")
+})
+/*const express = require("express");
 const youtubedl = require("youtube-dl-exec");
 const path = require("path");
 const fs = require("fs");
@@ -43,3 +74,4 @@ app.post("/downall", async (req, res) => {
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+*/
