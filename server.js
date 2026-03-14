d4f0a64f25d4a35e3359c0ca9989f73669aa960f
VLJNH-VN <vljnh5326@gmail.com>
2026-03-14 21:59:07 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
 const express = require("express")
-const { downloadVideo } = require("./downloader")
+const { y2mate } = require("./downloader")
 
 const app = express()
-
-app.get("/", (req,res)=>{
- res.json({status:"DOWNALL API RUNNING"})
-})
 
 app.get("/api", async (req,res)=>{
 
  const url = req.query.url
- if(!url) return res.json({error:"missing url"})
+
+ if(!url){
+  return res.json({error:"missing url"})
+ }
 
  try{
 
-  const data = await downloadVideo(url)
-  res.json(data)
+  const data = await y2mate(url)
+
+  res.json({
+   status:true,
+   data
+  })
 
  }catch(e){
 
-  res.json({error:"download failed"})
+  res.json({
+   error:"download failed"
+  })
 
  }
 
 })
 
-app.listen(process.env.PORT || 3000)
+app.listen(3000,()=>{
+ console.log("API running")
+})
