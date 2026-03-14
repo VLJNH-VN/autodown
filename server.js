625b849f76318b60ae739cb98bb9c2d17b90c8d2
VLJNH-VN <vljnh5326@gmail.com>
2026-03-14 22:11:43 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,71 @@
 const express = require("express")
-const { y2mate } = require("./downloader")
+const axios = require("axios")
 
 const app = express()
 
-app.get("/api", async (req,res)=>{
+async function y2mate(url) {
+    try {
 
- const url = req.query.url
+        const id = url.split("v=")[1].split("&")[0]
 
- if(!url){
-  return res.json({error:"missing url"})
- }
+        const analyze = await axios.post(
+            "https://www.y2mate.com/mates/analyzeV2/ajax",
+            `k_query=${url}&k_page=home&hl=en&q_auto=0`,
+            {
+                headers: {
+                    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                    "user-agent": "Mozilla/5.0"
+                }
+            }
+        )
 
- try{
+        const data = analyze.data
 
-  const data = await y2mate(url)
+        const videoId = data.vid
+        const token = data.token
 
-  res.json({
-   status:true,
-   data
-  })
+        const convert = await axios.post(
+            "https://www.y2mate.com/mates/convertV2/index",
+            `vid=${videoId}&k=${token}`,
+            {
+                headers: {
+                    "content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+                }
+            }
+        )
 
- }catch(e){
+        return {
+            title: data.title,
+            thumb: data.t,
+            link: convert.data.dlink
+        }
 
-  res.json({
-   error:"download failed"
-  })
+    } catch (e) {
+        return null
+    }
+}
 
- }
+app.get("/api/y2", async (req, res) => {
+
+    const url = req.query.url
+
+    if (!url) {
+        return res.json({ error: "missing url" })
+    }
+
+    const data = await y2mate(url)
+
+    if (!data) {
+        return res.json({ error: "download failed" })
+    }
+
+    res.json({
+        status: true,
+        result: data
+    })
 
 })
 
-app.listen(3000,()=>{
- console.log("API running")
+app.listen(3000, () => {
+    console.log("Y2 API running")
 })
