ed6201908c08b9a652ac97a09e2436e227568705
VLJNH-VN <vljnh5326@gmail.com>
2026-03-14 21:36:28 +0900

    Update downloader.js

--- downloader.js
+++ downloader.js
@@ -1,14 +1,24 @@
 const { exec } = require("child_process")
+const axios = require("axios")
 
-function downloadVideo(url){
+async function resolveUrl(url){
+ try{
+  const res = await axios.head(url, { maxRedirects: 5 })
+  return res.request.res.responseUrl || url
+ }catch{
+  return url
+ }
+}
+
+async function downloadVideo(url){
+
+ const realUrl = await resolveUrl(url)
+
  return new Promise((resolve,reject)=>{
 
-  exec(`yt-dlp -j "${url}"`, (err, stdout, stderr)=>{
+  exec(`yt-dlp -j --no-playlist "${realUrl}"`, (err, stdout, stderr)=>{
 
-   if(err){
-     console.log(stderr)
-     return reject(stderr)
-   }
+   if(err) return reject(stderr)
 
    const data = JSON.parse(stdout)
 
@@ -16,16 +26,16 @@ function downloadVideo(url){
    .filter(v => v.url)
    .slice(0,10)
    .map(v => ({
-     quality: v.format_note || v.height,
-     ext: v.ext,
-     url: v.url
+     quality:v.format_note || v.height,
+     ext:v.ext,
+     url:v.url
    }))
 
    resolve({
-     title:data.title,
-     thumbnail:data.thumbnail,
-     duration:data.duration,
-     formats
+    title:data.title,
+    thumbnail:data.thumbnail,
+    duration:data.duration,
+    formats
    })
 
   })
